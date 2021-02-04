a8e7dd6f8799e25d74daab21a716e0388a11a0fd
elreydetoda <alex@secureideas.com>
2021-02-04 01:31:15 -0500

    forgot the y...

--- base/packer/scripts/build/desktop-env.sh
+++ base/packer/scripts/build/desktop-env.sh
@@ -7,7 +7,7 @@ apt-get update
 
 apt-get -y install aufs-tools cgroupfs-mount mate-desktop-environment
 # removing because it gets installed by mate-desktop-environment
-apt remove --purge --auto-remove gnome-shell gdm3
+apt remove --purge --auto-remove -y gnome-shell gdm3
 # installing seperatly because of weird conflict with gdm3
 apt-get -y install lightdm --no-install-recommends
 
