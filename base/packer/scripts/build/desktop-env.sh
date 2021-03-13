65a3c42784c9dd2f0e9ba3d4f88e4311a3af33cb
elreydetoda <alex@secureideas.com>
2021-03-12 20:38:36 -0500

    removing no install recommends ( breaks lightdm )

--- base/packer/scripts/build/desktop-env.sh
+++ base/packer/scripts/build/desktop-env.sh
@@ -9,6 +9,6 @@ apt-get -y install aufs-tools cgroupfs-mount mate-desktop-environment
 # removing because it gets installed by mate-desktop-environment
 apt remove --purge --auto-remove -y gdm3 # gnome-shell
 # installing seperatly because of weird conflict with gdm3
-apt-get -y install lightdm --no-install-recommends
+apt-get -y install lightdm # --no-install-recommends
 
 reboot
